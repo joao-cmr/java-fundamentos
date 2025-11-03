227b25b9b25d45fc0595fd81e624bec6c508d221
joao-cmr <contatojoaocmr@gmail.com>
2025-11-03 13:10:15 -0300

    Variaveis Java

---
+++ FundamentosJava/src/aula02/variaveis.java
@@ -0,0 +1,13 @@
+package aula02;
+
+public class variaveis {
+
+    public static void main(String[] args) {
+
+        int idade=20;
+        String nome="João Carlos";
+
+        System.out.println("Meu nome é " + nome +  " e eu tenho " + idade + " anos!");
+
+    }
+}
